93c88fbb379665460b9750933b6601995f952d8c
janadoni <dhonicaptain718@gmail.com>
2025-08-27 13:04:58 +0530

    new project

---
+++ src/main/java/com/example/demo/controller.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class controller {
+    @GetMapping("/get")
+    public ResponseEntity<?> get(){
+        return new ResponseEntity<>("Hello new Deployment project", HttpStatus.OK);
+    }
+
+}
